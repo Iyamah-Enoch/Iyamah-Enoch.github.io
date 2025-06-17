888989d9ce47d3f2c49ab252e2706d111bd65755
Iyamah Enoch <76995667+Iyamah-Enoch@users.noreply.github.com>
2025-06-17 18:50:03 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const text = document.getElementById('text');
-
-text.addEventListener('mouseover', function handleMouseOver() {
-    text.style.color = 'gold';
-});
-
-text.addEventListener('mouseout', function handleMouseOut() {
-    text.style.color = 'white';
-});
